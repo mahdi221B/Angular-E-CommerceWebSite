907967fca2c52da0a860559c978a1adcb4cb946b
mahdi221B <mahdi.dridi@esprit.tn>
2023-02-01 10:41:54 +0100

    first commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Products } from '../core/model/products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  title!:string;
+  listProdcut!:Products[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.title="yello :)";
+    this.listProdcut=[
+      {id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
+      {id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
+      {id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0}, ]
+  }
+
+}
