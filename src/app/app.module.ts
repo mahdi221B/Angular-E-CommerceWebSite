82b3d9eff2f0b1a1664622bf7c1cf9dee862c0e3
mahdi221B <mahdi.dridi@esprit.tn>
2023-02-06 17:59:13 +0100

    Atelier n°2 - Manipulation de composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    OffresEmploiComponentComponent,
+    ArticlesComponentComponent
   ],
   imports: [
     BrowserModule,
