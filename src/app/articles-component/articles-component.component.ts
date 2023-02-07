93fc39d27ba3e56b1c4f4f92c984009fb1768e8c
mahdi221B <mahdi.dridi@esprit.tn>
2023-02-07 01:57:09 +0100

    Atelier2.1

--- src/app/articles-component/articles-component.component.ts
+++ src/app/articles-component/articles-component.component.ts
@@ -12,6 +12,7 @@ export class ArticlesComponentComponent implements OnInit {
   titre:string="Les articles du jour";
   listArticles!:Article[]
   num!:number;
+  itere!:number;
   constructor() { }
 
   ngOnInit(): void {
@@ -19,6 +20,7 @@ export class ArticlesComponentComponent implements OnInit {
       { titre: 'Le championnat du monde', contenu: 'Le champion du monde de cette année est', auteur: 'Med Taher', date: '12/12/2005', categorie: 'Sport' },
       { titre: 'Les nouveaux parents', contenu: 'Les nouveaux parents.', auteur: 'Ahmed Said.', date: '11/11/2018', categorie: 'Education' },
       { titre: 'Comment écrire votre CV.', contenu: 'Pour réussir à décraucher un emploi ...', auteur: 'Elsa.', date: '02/04/2017', categorie: 'Travain' }]}
+      
       public count() {
             let compteur:number=0;
             this.listArticles.forEach(element => {
