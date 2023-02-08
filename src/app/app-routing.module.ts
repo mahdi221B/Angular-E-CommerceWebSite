caf83e49b3be8082403872530aa08f98249398b5
mahdi221B <mahdi.dridi@esprit.tn>
2023-02-08 11:08:24 +0100

    Implémentation d’un système de navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { ProductDetailComponentComponent } from './product-detail-component/product-detail-component.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'products',component:ProductsComponent},
+  { path: 'products/:id', component: ProductDetailComponentComponent},
+  {path:'offres',component:OffresEmploiComponentComponent},
+  {path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponentComponent } from './product-detail-component/product-detail-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     OffresEmploiComponentComponent,
-    ArticlesComponentComponent
+    ArticlesComponentComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    HomeComponent,
+    ProductDetailComponentComponent
   ],
   imports: [
     BrowserModule,
